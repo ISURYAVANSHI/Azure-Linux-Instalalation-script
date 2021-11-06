180e1ee25d67d16a64ae43057d39b718fc3c2ff4
ISURYAVANSHI <92204581+ISURYAVANSHI@users.noreply.github.com>
2021-11-06 16:44:14 +0800

    Update InstallScript_v2.sh

--- InstallScript_v2.sh
+++ InstallScript_v2.sh
@@ -4,13 +4,13 @@ sudo apt-get update
 sudo apt-get -y install apache2
 sudo -i
 sudo systemctl start apache2
-sudo apt-get install php
-sudo apt-get  install php7.2-curl
-sudo apt-get install php7.2-cli
-sudo apt-get install php7.2-xml
+sudo apt-get -y install php
+sudo apt-get -y install php7.2-curl
+sudo apt-get -y install php7.2-cli
+sudo apt-get -y install php7.2-xml
 sudo chmod a+w /var/www/html
 cd /var/www/html
-sudo apt-get install composer
+sudo apt-get -y install composer
 sudo composer update
 cd /
 curl https://raw.githubusercontent.com/ISURYAVANSHI/PHPFiles/main/MyPhotos.php > /var/www/html/MyPhotos.php
