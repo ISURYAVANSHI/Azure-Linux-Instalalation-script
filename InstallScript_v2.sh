315b13911b931ed8771c969160cdf4e3fd09c626
ISURYAVANSHI <92204581+ISURYAVANSHI@users.noreply.github.com>
2021-11-06 16:18:56 +0800

    Update InstallScript_v2.sh

--- InstallScript_v2.sh
+++ InstallScript_v2.sh
@@ -6,9 +6,8 @@ sudo -i
 sudo systemctl start apache2
 sudo apt-get install php
 sudo apt-get  install php7.2-curl
-sudo apt-get install php7.2-xml
 sudo apt-get install php7.2-cli
-sudo apt-get  install composer
+sudo apt-get install php7.2-xml
 sudo chmod a+w /var/www/html
 cd /var/www/html
 sudo apt-get install composer
@@ -16,4 +15,3 @@ sudo composer update
 cd /
 curl https://raw.githubusercontent.com/ISURYAVANSHI/PHPFiles/main/MyPhotos.php > /var/www/html/MyPhotos.php
 curl https://raw.githubusercontent.com/ISURYAVANSHI/PHPFiles/main/composer.json > /var/www/html/composer.json
-
